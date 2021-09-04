caf429a049ff6cd485a81146316769d1df397e5e
dfuseop-exe <sushants2418@gmail.com>
2021-09-04 17:12:44 +0530

    Adding Loading spinner

--- src/components/News.js
+++ src/components/News.js
@@ -1,75 +1,114 @@
 import React, { Component } from "react";
 import Newsitem from "./Newsitem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
-
-  Handlenextclick = async ()=>{
-    if(this.state.page + 1  > Math.ceil(this.state.totalResults/20)){
-
+  Handlenextclick = async () => {
+    if (
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
+    ) {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=56c28c132f5a419f867a3ca6727aadfe&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({loading : true})
+      let data = await fetch(url);
+      let parsedata = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedata.articles,
+        loading : false
+      });
     }
-    else{
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=56c28c132f5a419f867a3ca6727aadfe&page=${this.state.page + 1 }&pageSize=20` ;
-    let data = await fetch(url);
-    let parsedata = await data.json();
-    this.setState({
-      page : this.state.page + 1 ,
-      articles : parsedata.articles ,
-    })
-    } 
-  }
+  };
 
-  Handlepreclick = async ()=>{
-    
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=56c28c132f5a419f867a3ca6727aadfe&page=${this.state.page - 1 }&pageSize=20` ;
+  Handlepreclick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=56c28c132f5a419f867a3ca6727aadfe&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({loading : true})
     let data = await fetch(url);
     let parsedata = await data.json();
     this.setState({
-      page : this.state.page - 1 ,
-      articles : parsedata.articles
-    })
-  }
-  
+      page: this.state.page - 1,
+      articles: parsedata.articles,
+      loading : false
+    });
+  };
+
   constructor() {
     //run before render
     super();
-    this.state = { 
+    this.state = {
       articles: [],
       loading: false,
-      page : 1
+      page: 1,
     };
   }
 
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=56c28c132f5a419f867a3ca6727aadfe&page=1" ;
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=56c28c132f5a419f867a3ca6727aadfe&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading : true})
     let data = await fetch(url);
     let parsedata = await data.json();
     console.log(parsedata);
-    this.setState({articles : parsedata.articles , totalResults : parsedata.totalResults });
+    this.setState({
+      articles: parsedata.articles,
+      totalResults: parsedata.totalResults,
+      loading : false
+    });
   }
 
   render() {
     return (
       <div className="container my-3 ">
-          <h1 className="text-center">Updates-Rocket Top Headlines</h1>
-          <div className="row ">
-          {this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <Newsitem
-                    title={element.title ? element.title : "Default Title"}
-                    description={element.description ?element.description : "Default Desc"}
-                    imageurl={element.urlToImage? element.urlToImage : "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"}
-                    newsurl = {element.url}
-                  />
-                </div>
-              );
-            })}
-          </div>
-          <div className="container d-flex justify-content-evenly">
-          <button type="button" disabled={this.state.page <=1} class="btn btn-dark" onClick={this.Handlepreclick}> &larr; Previous</button>
-          <button disabled={this.state.page + 1  > Math.ceil(this.state.totalResults/20)} type="button"  class="btn btn-dark" onClick={this.Handlenextclick}>Next &rarr;</button>
-          </div>
+        <h1 className="text-center">Updates-Rocket Top Headlines</h1>
+        {this.state.loading && <Spinner />}
+        <div className="row ">
+          {!this.state.loading && this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <Newsitem
+                  title={element.title ? element.title : "Default Title"}
+                  description={
+                    element.description ? element.description : "Default Desc"
+                  }
+                  imageurl={
+                    element.urlToImage
+                      ? element.urlToImage
+                      : "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
+                  }
+                  newsurl={element.url}
+                />
+              </div>
+            );
+          })}
         </div>
+        <div className="container d-flex justify-content-evenly">
+          <button
+            type="button"
+            disabled={this.state.page <= 1}
+            class="btn btn-dark"
+            onClick={this.Handlepreclick}
+          >
+            {" "}
+            &larr; Previous
+          </button>
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            type="button"
+            class="btn btn-dark"
+            onClick={this.Handlenextclick}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
     );
   }
 }
